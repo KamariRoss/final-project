6e0f2bb32c00f7d8658fde03737543197311cb1c
KamariRoss <kamarimobile@gmail.com>
2020-03-21 19:38:22 -0400

    Added basic Homepage Html

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
 
-const App = () => {
-  return <HelloWorld />
+class App extends React.Component {
+  render() {
+    return (
+      <article>
+        <header></header>
+        <section>
+          <section>
+            <button type="button">Activate</button>
+          </section>
+        </section>
+        <footer>
+          <nav>
+            <ul>
+              <li>
+                <a href="https://www.mozilla.org/en-US/">Rest </a>
+              </li>
+              <li>
+                <a href="https://www.mozilla.org/en-US/">Stats </a>
+              </li>
+              <li>
+                <a href="https://www.mozilla.org/en-US/">Pref </a>
+              </li>
+            </ul>
+          </nav>
+        </footer>
+      </article>
+    )
+  }
 }
 
 export default App
